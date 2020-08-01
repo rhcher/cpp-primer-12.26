16d9fc17afe4bc2846f4df60cfae9721229bafd8
rhcher <2032877541@qq.com>
2020-08-01 19:09:55 +0800

    9.51

--- include/Screen.hpp
+++ include/Screen.hpp
@@ -1,88 +1,37 @@
 #pragma once
-#include <iostream>
 #include <string>
-#include <vector>
+#include <iostream>
+#include <sstream>
 
-class Screen
-{
-	friend class Window_mgr;
-
-public:
-	using pos = std::string::size_type;
-
-	Screen() = default;
-	Screen(pos ht, pos wd, char c)
-		: height(ht), width(wd), contents(ht * wd, c) {}
-	Screen(pos ht, pos wd)
-		: Screen(ht, wd, ' ') {}
-	char get() const
-	{
-		return contents[cursor];
-	}
-	inline char get(pos ht, pos wd) const;
-	Screen& move(pos r, pos c);
-	Screen& set(char);
-	Screen& set(pos, pos, char);
-
-	Screen& display(std::ostream& os)
-	{
-		do_display(os);
-		return *this;
-	}
-	const Screen& display(std::ostream& os) const
-	{
-		do_display(os);
-		return *this;
-	}
-
-private:
-	pos cursor = 0;
-	pos height = 0;
-	pos width = 0;
-	std::string contents;
-
-	void do_display(std::ostream& os) const
-	{
-		for (decltype(cursor) index = 0; index < height; ++index)
-		{
-			os << contents.substr(index * width, width);
-			os << std::endl;
-		}
-	}
-};
-
-inline Screen& Screen::move(pos r, pos c)
-{
-	pos row = (r - 1) * width;
-	cursor = row + c;
-	return *this;
-}
-
-char Screen::get(pos ht, pos wd) const
-{
-	pos row = ht * width;
-	return contents[row + wd];
-}
-
-Screen& Screen::set(char c)
-{
-	contents[cursor] = c;
-	return *this;
-}
-
-Screen& Screen::set(pos r, pos col, char c)
-{
-	contents[r * width + col] = c;
-	return *this;
-}
-
-class Window_mgr
+class Date
 {
 public:
-	using ScreenIndex = std::vector<Screen>::size_type;
-	void clear(ScreenIndex);
-	ScreenIndex addScreen(const Screen&);
+	Date() = default;
+	Date(std::string date)
+	{
+		// 1/1/1990
+        std::string tmp= date.substr(date.find_first_of(number));
+        day = std::stoi(tmp);
+        tmp = tmp.substr(tmp.find_first_not_of(number));
+        tmp = tmp.substr(tmp.find_first_of(number));
+        month = std::stoi(tmp);
+        tmp = tmp.substr(tmp.find_first_not_of(number));
+        tmp = tmp.substr(tmp.find_first_of(number));
+        year = std::stoi(tmp);
+	}
+	void date_show()
+	{
+		std::cout << "year: " << year << std::endl;
+		std::cout << "month: " << month << std::endl;
+		std::cout << "day: " << day << std::endl;
+	}
+	virtual ~Date() {}
 
 private:
-	std::vector<Screen> screens{Screen(24, 80, ' ')};
+	unsigned year;
+	unsigned month;
+	unsigned day;
+    static const std::string number;
 };
+
+const std::string Date::number {"0123456789"};
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,38 @@
+#include <stack>
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
-
-void Foo(string& s, const string& asc, const string& desc)
-{
-	string::difference_type diff = 0;
-	string::size_type ascpos = 0;
-	string::size_type descpos = 0;
-	vector<string> result{};
-
-	while ((ascpos = s.find_first_of(asc, ascpos)) != string::npos && (descpos = s.find_first_of(desc, descpos)) != string::npos)
-	{
-		if (ascpos < descpos)
-		{
-			result.push_back(s.substr(ascpos+1, descpos-1));
-		}
-		else
-		{
-			result.push_back(s.substr(descpos+1, ascpos-1));
-		}
-		++ascpos;
-		++descpos;
-	}
-
-	for (auto i = result.cbegin(); i != result.cend(); ++i)
-	{
-		size_t max = (*i).size();
-		for (auto iter = i + 1; iter != result.cend();)
-		{
-			if (max < (*iter).size())
-			{
-				result.insert(result.cbegin(), *iter);
-				iter = result.erase(iter);
-			}
-			++iter;
-		}
-	}
-
-	cout << "the result is : " <<result.at(0)<<endl;
-}
 
 int main(int argc, char* argv[])
 {
-	string ascender{"bdfhklt"};
-	string descender{"gjpqy"};
-	string s{};
-	cout << "Please input a string : " << endl;
-	cin >> s;
-	Foo(s, ascender, descender);
+	string expr{"1*(2+3)"};
+	stack<char> c_stack;
+	auto seen = 0;
+	auto rep = '#';
+	for (auto item : expr)
+	{
+		c_stack.push(item);
+		if (c_stack.top() == '(')
+		{
+			++seen;
+		}
+		if (seen && item == ')')
+		{
+			while (c_stack.top() != '(')
+			{
+				c_stack.pop();
+			}
+			c_stack.pop();
+			c_stack.push(rep);
+			--seen;
+		}
+	}
+
+	string output{};
+	for (; !c_stack.empty(); c_stack.pop())
+	{
+		output.insert(output.begin(), c_stack.top());
+	}
+	cout << output << endl;
+	return EXIT_SUCCESS;
 }
